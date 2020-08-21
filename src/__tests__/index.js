f0eee3d6319af675fc227bb55f14c5edd090f9b4
Jeffrey Lu <yumyumyum@yumyumythership.fios-router.home>
2020-08-20 22:00:32 -0400

    added github action

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,5 +1,5 @@
 const puppeteer = require("puppeteer");
-const reactPinpoint = require('../react-pinpoint');
+const reactPinpoint = require('react-pinpoint');
 
 let browser, page
 
@@ -11,7 +11,7 @@ beforeEach(async () => {
   page = await browser.newPage();
 
   // If tests need different root or url then we need to put this inside the tests
-  const url = "http://localhost:3000/calculator"
+  const url = "http://webapp:3000/calculator"
   const rootId = "#root"
   await reactPinpoint.recordTest(page, url, rootId)
 })
@@ -19,8 +19,8 @@ beforeEach(async () => {
 
 
 afterEach(async () => {
-  const slowRenders = await reactPinpoint.reportTestResults(page)
-  // console.log("YEAH->", slowRenders);
+  const slowRenders = await reactPinpoint.reportTestResults(page, 16)
+  console.log("Slow Renders:", slowRenders);
   await browser.close();
 })
 
@@ -40,34 +40,32 @@ test("999 displays", async () => {
 })
 
 
-test("666 displays", async () => {
-  await page.click("#yeah6");
-  await page.click("#yeah6");
-  await page.click("#yeah6");
+// test("666 displays", async () => {
+//   await page.click("#yeah6");
+//   await page.click("#yeah6");
+//   await page.click("#yeah6");
 
-  const value = await page.evaluate(() => {
-    const element = document.querySelector(".component-display>div")
-    return element.textContent
-  })
+//   const value = await page.evaluate(() => {
+//     const element = document.querySelector(".component-display>div")
+//     return element.textContent
+//   })
 
-  expect(value).toBe("666")
-})
-
-
-
-test("Test add", async () => {
-  await page.click("#yeah9");
-  await page.click("#yeah\\+");
-  await page.click("#yeah9");
-  await page.click("#yeah\\=");
-
-  const value = await page.evaluate(() => {
-    const element = document.querySelector(".component-display>div")
-    return element.textContent
-  })
-
-  expect(value).toBe("18")
-
-})
+//   expect(value).toBe("666")
+// })
 
 
+
+// test("Test add", async () => {
+//   await page.click("#yeah9");
+//   await page.click("#yeah\\+");
+//   await page.click("#yeah9");
+//   await page.click("#yeah\\=");
+
+//   const value = await page.evaluate(() => {
+//     const element = document.querySelector(".component-display>div")
+//     return element.textContent
+//   })
+
+//   expect(value).toBe("18")
+
+// })
